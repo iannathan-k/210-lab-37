6166c371394cdc73e402a028d48646ea504c4a1c
iannathan-k <ianmtv@gmail.com>
2026-05-14 14:54:17 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 // COMSC-210 | Lab 37 | Ian Kusmiantoro
@@ -7,10 +8,26 @@ using namespace std;
 int sum_ascii(string);
 
 int main() {
-    cout << sum_ascii("536B9DFC93AF") << endl;
-    cout << sum_ascii("1DA9D64D02A0") << endl;
-    cout << sum_ascii("666D109AA22E") << endl;
-    cout << sum_ascii("E1D2665B21EA") << endl;
+    ifstream fin;
+    fin.open("data.txt");
+
+    // Guard clause
+    if (!fin.good()) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    // Sum each line and add to total
+    string code;
+    int total = 0;
+    while (fin >> code) {
+        total += sum_ascii(code);
+    }
+
+    cout << "Total sum: " << total << endl;
+    // yay I get 69893419
+
+    fin.close();
 
     return 0;
 }
