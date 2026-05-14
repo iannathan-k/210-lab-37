29ae7010a9cc90200c70c3c516dd08546de57b18
iannathan-k <ianmtv@gmail.com>
2026-05-14 14:50:06 -0700

    milestone 1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // COMSC-210 | Lab 37 | Ian Kusmiantoro
 
+int sum_ascii(string);
+
 int main() {
-    char a = 'A';
-    cout << a << endl;
-    cout << (int) a << endl;
-    int b = 66;
-    cout << b << endl;
-    cout << (char) b << endl;
-    
+    cout << sum_ascii("536B9DFC93AF") << endl;
+    cout << sum_ascii("1DA9D64D02A0") << endl;
+    cout << sum_ascii("666D109AA22E") << endl;
+    cout << sum_ascii("E1D2665B21EA") << endl;
 
     return 0;
+}
+
+int sum_ascii(string str) {
+    int sum = 0;
+
+    for (char c : str) {
+        sum += (int) c;
+    }
+
+    return sum;
 }
 
 /* 
